3d9bdde18a4c279e6bdab554770dddbf05abfef2
Holly Tunison <hollytunison@gmail.com>
2021-12-13 10:38:50 -0500

    added state hooks to functions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AddTask from './components/AddTask'
 
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState (false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -26,6 +27,14 @@ const App = () => {
     },
   ])
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+  console.log(id)
+}
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !==id))
@@ -38,17 +47,21 @@ const toggleReminder = (id) => {
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask />
-      {tasks.length > 0 ? (
-      <Tasks 
-      tasks={tasks} 
-      onDelete={deleteTask}
-      onToggle={toggleReminder}
-      /> 
-      ) : (
-      'No tasks to show'
-      )}
+      <Header 
+      onAdd={() => setShowAddTask
+      (!showAddTask)}
+      showAdd={showAddTask} 
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  'No Tasks To Show'
+                )}
     </div>
   )
 }
