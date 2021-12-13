13531919b63a1f19c0e035b774a3624d9494ecea
Holly Tunison <hollytunison@gmail.com>
2021-12-13 09:20:38 -0500

    added components and form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
-function App() {
+import { useState } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask'
+
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Dr Appt',
+      day: 'May 5th',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Car Appt',
+      day: 'May 5th',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Hair Appt',
+      day: 'May 5th',
+      reminder: true
+    },
+  ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !==id))
+  }
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+}
+
   return (
     <div className='container'>
-      <h1>Hello from React</h1>
+      <Header />
+      <AddTask />
+      {tasks.length > 0 ? (
+      <Tasks 
+      tasks={tasks} 
+      onDelete={deleteTask}
+      onToggle={toggleReminder}
+      /> 
+      ) : (
+      'No tasks to show'
+      )}
     </div>
-  );
+  )
 }
+
+// FOR CLASS COMPONENTS
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
